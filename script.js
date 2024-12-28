cb21ca902e8b3855f60cdf0a6960f5cdf78a86e5
simplymas <108439627+simplymas@users.noreply.github.com>
2024-12-28 11:07:41 -0600

    silly fix

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ const content = document.getElementById('content');
 
 // check if the device is in landscape mode
 function checkOrientation() {
-  const isLandscape = window.innerWidth > window.innerHeight;
-
-  if (isLandscape) {
-    // hide notif and show
-    orientationNotification.style.display = 'none';
-    content.style.visibility = 'visible';
-  } else {
-    // show and hide content
-    notificationText.textContent = window.innerWidth < window.innerHeight
-      ? 'Please put your phone horizontally so you can see :3'
-      : 'hey, put it back 3:<';
-    orientationNotification.style.display = 'flex';
-    content.style.visibility = 'hidden';
+    const isLandscape = window.innerWidth > window.innerHeight;
+  
+    if (isLandscape) {
+      // hide notif and show
+      orientationNotification.style.display = 'none';
+      orientationNotification.style.backgroundColor = 'transparent'; // no background
+      content.style.visibility = 'visible';
+    } else {
+      // show notif and hide web
+      notificationText.textContent = 'Please put your phone horizontally so you can see :3';
+      orientationNotification.style.display = 'flex';
+      orientationNotification.style.backgroundColor = 'rgba(0, 0, 0, 0.9)'; // dim effect
+      content.style.visibility = 'hidden';
+    }
   }
-}
 
 // watch the resize
 window.addEventListener('resize', checkOrientation);
