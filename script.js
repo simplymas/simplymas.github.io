e5717ff40be27a549558a0295841109e9fbb88fb
simplymas <108439627+simplymas@users.noreply.github.com>
2024-12-30 15:53:12 -0600

    fix

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// notif and content
-//const orientationNotification = document.getElementById('orientationNotification');
-//const notificationText = document.getElementById('notificationText');
-//const content = document.getElementById('content');
+/*notif and content
+const orientationNotification = document.getElementById('orientationNotification');
+const notificationText = document.getElementById('notificationText');
+const content = document.getElementById('content');
 
-// check if the device is in landscape mode
-//function checkOrientation() {
-    //const isLandscape = window.innerWidth > window.innerHeight;
+check if the device is in landscape mode
+function checkOrientation() {
+    const isLandscape = window.innerWidth > window.innerHeight;
   
-    //if (isLandscape) {
-      // hide notif w fade
-      //setTimeout(() => {
-        //orientationNotification.classList.remove('show-notification');
-      //}, 500); // fade first then hide
-      //content.style.visibility = 'visible';
-    //} else {
-      // show notif dim rest
-      //notificationText.textContent = 'Please put your phone horizontally so you can see :3';
-      //orientationNotification.classList.add('show-notification');
-      //orientationNotification.style.opacity = '1'; // let there be light
-      //content.style.visibility = 'hidden';
-    //}
-  //}
+    if (isLandscape) {
+      hide notif w fade
+      setTimeout(() => {
+        orientationNotification.classList.remove('show-notification');
+      }, 500); // fade first then hide
+      content.style.visibility = 'visible';
+    } else {
+       show notif dim rest
+      notificationText.textContent = 'Please put your phone horizontally so you can see :3';
+      orientationNotification.classList.add('show-notification');
+      orientationNotification.style.opacity = '1'; // let there be light
+      content.style.visibility = 'hidden';
+    }
+  }
 
-// watch the resize
-//window.addEventListener('resize', checkOrientation);
-//wind//ow.addEventListener('orientationchange', checkOrientation);
+ watch the resize
+window.addEventListener('resize', checkOrientation);
+wind//ow.addEventListener('orientationchange', checkOrientation);
 
-// page load
-//window.onload = checkOrientation;
+ page load
+window.onload = checkOrientation;*/
