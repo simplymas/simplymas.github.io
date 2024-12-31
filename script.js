acc50229b8101c0dfef4fccab087eee406562dec
simplymas <108439627+simplymas@users.noreply.github.com>
2024-12-30 19:28:43 -0600

    Release 1.0.0
    First release of my website, enjoy :3

--- script.js
+++ script.js
@@ -28,3 +28,22 @@ wind//ow.addEventListener('orientationchange', checkOrientation);
 
  page load
 window.onload = checkOrientation;*/
+
+function updateTime() {
+  const currentTime = new Date();
+  
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
+  
+  hours = (hours < 10) ? '0' + hours : hours;
+  minutes = (minutes < 10) ? '0' + minutes : minutes;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+  const timeString = hours + ':' + minutes + ':' + seconds;
+  document.getElementById('time').textContent = timeString;
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
